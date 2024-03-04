89a78030afe73e515a824e6c1ae4b85d6105f3b4
Bailey <baileywatts74@gmail.com>
2024-03-04 12:33:42 +1000

    message

---
+++ src/AuthenticationService.java
@@ -0,0 +1,27 @@
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    // TODO Now: Add a constructor to initialize the users list with the default user
+    public AuthenticationService(){
+        users.add(new User("test", "test"));
+    }
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    public User signUp(String username, String password){
+        if (usernameTaken(username)){
+            return null;
+        }
+        User newUser = new User(username,password);
+        user.add(newUser);
+        return newUser;
+    }
+
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    public User logIn(String username, String password){
+        for (User user : users){
+            if (user.getUsername().equals(username)) && user.getPassword().equals(password)){
+                return true;
+            }
+        }
+    }
+}
